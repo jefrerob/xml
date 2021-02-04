65badbbd70f094850a9d8af1e67eaa2b6f719bce
JefreRobert <jefre.robert@vdabcampus.be>
2021-02-04 14:22:56 +0100

    a commit

--- src/main/java/be/vdab/xml/XmlApplication.java
+++ src/main/java/be/vdab/xml/XmlApplication.java
@@ -2,8 +2,10 @@ package be.vdab.xml;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
+@ImportResource("beans.xml")
 public class XmlApplication {
 
     public static void main(String[] args) {
---
+++ src/main/java/be/vdab/xml/services/DefaultKwadraatService.java
@@ -0,0 +1,12 @@
+package be.vdab.xml.services;
+
+public class DefaultKwadraatService implements KwadraatService {
+
+    @Override
+    public int kwadraat(int getal){
+        return getal * getal;
+    }
+
+
+
+}
